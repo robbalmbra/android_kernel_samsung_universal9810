8d21efe57b418067e8c6152bbd37012db94cc087
Ihabov Marlov <32247935+THEBOSS619@users.noreply.github.com>
2020-06-25 01:56:22 +0300

    sec_cmd: instead of passing nonsense string just prevent abusing strncat

--- drivers/input/sec_cmd.c
+++ drivers/input/sec_cmd.c
@@ -61,8 +61,6 @@ void sec_cmd_set_default_result(struct sec_cmd_data *data)
 
 void sec_cmd_set_cmd_result_all(struct sec_cmd_data *data, char *buff, int len, char *item)
 {
-	char delim1 = ' ';
-	char delim2 = ':';
 	int cmd_result_len;
 
 	cmd_result_len = (int)strlen(data->cmd_result_all) + len + 2 + (int)strlen(item);
@@ -73,9 +71,9 @@ void sec_cmd_set_cmd_result_all(struct sec_cmd_data *data, char *buff, int len, 
 	}
 
 	data->item_count++;
-	strncat(data->cmd_result_all, &delim1, 1);
+	strcat(data->cmd_result_all, " ");
 	strncat(data->cmd_result_all, item, strlen(item));
-	strncat(data->cmd_result_all, &delim2, 1);
+	strcat(data->cmd_result_all, ":");
 	strncat(data->cmd_result_all, buff, len);
 }
 
